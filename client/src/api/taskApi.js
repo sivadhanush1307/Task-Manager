64b253b36d53b56479470958c34ccee8ab4cfcc2
sivadhanush1307 <sdhanush129@gmail.com>
2025-12-15 22:21:50 +0530

    updated

--- client/src/api/taskApi.js
+++ client/src/api/taskApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-backend-ibgo.onrender.com",
 });
 
 export const fetchTasks = () => API.get("/tasks");
